0013c8fa3802d220ab1067e2410b0c208bfc9e99
Ntsikelelo Metseeme <41383664+Ntsikelel@users.noreply.github.com>
2020-04-04 16:27:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.carousel').carousel()
+
 (function($) {
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
